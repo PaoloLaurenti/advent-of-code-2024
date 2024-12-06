c5298ac48a2a35a9af20bff40cc240f41fc3aacb
Paolo Laurenti <laurentipaolo@gmail.com>
2024-12-06 15:03:19 +0100

    complete Day 3 second part: Mull it Over

--- lib/Day_3/mull_it_over.ex
+++ lib/Day_3/mull_it_over.ex
@@ -2,9 +2,21 @@ defmodule Day3.MullItOver do
   @regex ~r/mul\((\d{1,3}),(\d{1,3})\)/
 
   def calculate(file_path) do
-    {:ok, text} = file_path
-    |> File.read()
-    calculate_mul(text)
+    file_path
+    |> read_input_text()
+    |> calculate_mul()
+  end
+
+  def calculate_with_do_dont(file_path) do
+    file_path
+    |> read_input_text()
+    |> String.replace("\n", "")
+    |> calculate_mul_with_do_dont()
+  end
+
+  defp read_input_text(file_path) do
+    {:ok, text} = File.read(file_path)
+    text
   end
 
   def calculate_mul(line) do
@@ -15,5 +27,26 @@ defmodule Day3.MullItOver do
       {mul2, _} = Integer.parse(n2)
       acc + mul1 * mul2
     end)
+  end
+
+  def calculate_mul_with_do_dont(line) do
+    [first_part | rest] = String.split(line, "don't()")
+
+    first_part_mul = calculate_mul(first_part)
+
+    Enum.reduce(rest, first_part_mul, fn part, sum ->
+      calculate_mul_with_do(part) + sum
+    end)
+  end
+
+  defp calculate_mul_with_do(text) do
+    if String.contains?(text, "do()") do
+      text
+      |> String.split("do()", parts: 2)
+      |> Enum.at(1)
+      |> calculate_mul()
+    else
+      0
+    end
   end
 end
--- test/Day_3/mull_it_over_test.exs
+++ test/Day_3/mull_it_over_test.exs
@@ -1,6 +1,7 @@
 defmodule Day3.MullItOverTest do
   use ExUnit.Case
-  describe "calculate sum of multiplication on multilines" do
+
+  describe "calculate sum of multiplication on multi-lines" do
     test "many muls" do
       result = Day3.MullItOver.calculate_mul("mul(4,2)\nmul(3,3)\nmul(2,1)")
       assert 19 == result
@@ -8,8 +9,12 @@ defmodule Day3.MullItOverTest do
 
     test "with input" do
       result = Day3.MullItOver.calculate("test/Day_3/input.txt")
-      IO.inspect(result)
-      # assert 19 == result
+      assert result == 174_960_292
+    end
+
+    test "with input with do don't" do
+      result = Day3.MullItOver.calculate_with_do_dont("test/Day_3/input.txt")
+      assert result == 56_275_602
     end
   end
 
@@ -70,6 +75,78 @@ defmodule Day3.MullItOverTest do
         result = Day3.MullItOver.calculate_mul(unquote(line))
         assert unquote(expected_result) == result
       end
+    end
+  end
+
+  describe "calculate sum of multiplication with do/don't on a single line with" do
+    test "muls with no do/don't" do
+      result = Day3.MullItOver.calculate_mul_with_do_dont("mul(42,212)")
+      assert 8904 == result
+    end
+
+    test "mulls with one single following don't and no mul after" do
+      result = Day3.MullItOver.calculate_mul_with_do_dont("mul(42,212)afdsdon't()")
+      assert 8904 == result
+    end
+
+    test "mulls with one single following don't and muls after" do
+      result = Day3.MullItOver.calculate_mul_with_do_dont("mul(42,212)afdsdon't()mul(3,4)")
+      assert 8904 == result
+    end
+
+    test "mulls with one single following don't, no mul after and then a do with no muls after" do
+      result = Day3.MullItOver.calculate_mul_with_do_dont("mul(42,212)afdsdon't()sdfdsfdo()")
+      assert 8904 == result
+    end
+
+    test "mulls with one single following don't, muls after and then a do with no muls after" do
+      result =
+        Day3.MullItOver.calculate_mul_with_do_dont("mul(42,212)afdsdon't()mul(4,5)sdfdsfdo()")
+
+      assert 8904 == result
+    end
+
+    test "mulls with one single following don't, no mul after and then a do with muls after" do
+      result =
+        Day3.MullItOver.calculate_mul_with_do_dont("mul(42,212)afdsdon't())sdfdsfdo()mul(4,5)")
+
+      assert 8924 == result
+    end
+
+    test "mulls with one single following don't, muls after and then a do with muls after" do
+      result =
+        Day3.MullItOver.calculate_mul_with_do_dont(
+          "mul(42,212)afdsdon't())smul(2,3)dfdsfdo()mul(4,5)"
+        )
+
+      assert 8924 == result
+    end
+
+    test "following don'ts with no do's in the middle" do
+      result =
+        Day3.MullItOver.calculate_mul_with_do_dont(
+          "mul(42,212)afdsdon't())smul(2,3)dfdsfdon't()mul(4,5)"
+        )
+
+      assert 8904 == result
+    end
+
+    test "following do with no don't's in the middle" do
+      result =
+        Day3.MullItOver.calculate_mul_with_do_dont(
+          "mul(42,212)afdsdon't())smul(2,3)dfdsfdo()sdfsdgdo()mul(4,5)"
+        )
+
+      assert 8924 == result
+    end
+
+    test "example" do
+      result =
+        Day3.MullItOver.calculate_mul_with_do_dont(
+          "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        )
+
+      assert 48 == result
     end
   end
 end
