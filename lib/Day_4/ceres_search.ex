89aa311c953b21492a4e1ee57eb51cf5a1691c02
Paolo Laurenti <laurentipaolo@gmail.com>
2024-12-11 18:06:52 +0100

    complete Day 4 first part: Ceres Search

--- lib/Day_4/ceres_search.ex
+++ lib/Day_4/ceres_search.ex
@@ -1,5 +1,21 @@
 defmodule Day4.CeresSearch do
+  def count_xmas_from_file(file_path) do
+    {:ok, text} = File.read(file_path)
+
+    text
+    |> String.split("\n")
+    |> Enum.with_index()
+    |> Enum.map(fn {row, idx} -> {idx, String.split(row, "", trim: true)} end)
+    |> Enum.into(%{})
+    |> count_xmas()
+  end
+
   def count_xmas(map) do
+    map_info = %{
+      height: map |> Map.keys() |> length,
+      width: length(Map.get(map, 0, []))
+    }
+
     {right_to_left_count, x_coordinates} =
       map
       |> Enum.reduce({0, []}, fn {row_ix, row}, {sum, x_coordinates} ->
@@ -7,32 +23,136 @@ defmodule Day4.CeresSearch do
           row
           |> Enum.with_index()
           |> Enum.filter(fn {value, _ix} -> value == "X" end)
-          |> Enum.map(fn {_value, ix} -> {row_ix, ix} end)
+          |> Enum.map(fn {_value, ix} -> {ix, row_ix} end)
 
         x_coordinates = Enum.concat(x_coordinates, row_x_coordinates)
-        sum = sum + count_in_line(row)
+        sum = sum + count_in_line(row, ~r/XMAS/)
         {sum, x_coordinates}
       end)
 
-    bottom_right_diagonal_count =
-      x_coordinates
-      |> Enum.map(&get_bottom_right_diagonal(map, &1))
-      |> Enum.map(&count_in_line(&1))
-      |> Enum.sum()
+    [
+      &get_bottom_right_diagonal_line/3,
+      &get_top_bottom_line/3,
+      &get_bottom_left_diagonal_line/3,
+      &get_right_to_left_line/3,
+      &get_top_left_diagonal_line/3,
+      &get_bottom_top_line/3,
+      &get_top_right_diagonal_line/3
+    ]
+    |> Enum.reduce(right_to_left_count, fn fn_get_line, partial_sum ->
+      line_count =
+        x_coordinates
+        |> Enum.map(&fn_get_line.(map, map_info, &1))
+        |> Enum.map(&count_in_line(&1, ~r/^XMAS/))
+        |> Enum.sum()
 
-    right_to_left_count + bottom_right_diagonal_count
+      partial_sum + line_count
+    end)
   end
 
-  defp count_in_line(line) do
+  defp count_in_line(line, regex) do
     line_text = Enum.join(line)
 
-    Regex.scan(~r/XMAS/, line_text, capture: :all_but_first)
+    Regex.scan(regex, line_text, capture: :all_but_first)
     |> Enum.count()
   end
 
-  defp get_bottom_right_diagonal(map, {x, y} = _x_coordinate) do
-    height = map |> Map.keys() |> length
-    width = length(Map.get(map, x))
-    
+  defp get_bottom_right_diagonal_line(
+         map,
+         %{height: height, width: width},
+         {x, y} = _x_coordinate
+       ) do
+    Enum.reduce_while(0..max(height, width), [], fn step, line ->
+      if x + step == width || y + step == height do
+        {:halt, line}
+      else
+        char = map |> Map.get(y + step, []) |> Enum.at(x + step)
+        {:cont, [char | line]}
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp get_top_bottom_line(map, %{height: height}, {x, y} = _x_coordinate) do
+    Enum.reduce_while(0..height, [], fn step, line ->
+      if y + step > height do
+        {:halt, line}
+      else
+        char = map |> Map.get(y + step, []) |> Enum.at(x)
+        {:cont, [char | line]}
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp get_bottom_left_diagonal_line(
+         map,
+         %{height: height, width: width},
+         {x, y} = _x_coordinate
+       ) do
+    Enum.reduce_while(0..max(height, width), [], fn step, line ->
+      next_x = if x - step < 0, do: 0, else: x - step
+
+      if x - step < 0 || y + step > height do
+        {:halt, line}
+      else
+        char = map |> Map.get(y + step, []) |> Enum.at(next_x)
+        {:cont, [char | line]}
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp get_right_to_left_line(map, _map_info, {x, y} = _x_coordinate) do
+    map |> Map.get(y) |> Enum.slice(0..x) |> Enum.reverse()
+  end
+
+  defp get_top_left_diagonal_line(
+         map,
+         %{height: height, width: width},
+         {x, y} = _x_coordinate
+       ) do
+    Enum.reduce_while(0..max(height, width), [], fn step, line ->
+      next_x = if x - step < 0, do: 0, else: x - step
+      next_y = if y - step < 0, do: 0, else: y - step
+
+      if x - step < 0 || y - step < 0 do
+        {:halt, line}
+      else
+        char = map |> Map.get(next_y, []) |> Enum.at(next_x)
+        {:cont, [char | line]}
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp get_bottom_top_line(map, _map_info, {x, y} = _x_coordinate) do
+    Enum.reduce_while(0..y, [], fn step, line ->
+      if y - step < 0 do
+        {:halt, line}
+      else
+        char = map |> Map.get(y - step, []) |> Enum.at(x)
+        {:cont, [char | line]}
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp get_top_right_diagonal_line(
+         map,
+         %{height: height, width: width},
+         {x, y} = _x_coordinate
+       ) do
+    Enum.reduce_while(0..max(height, width), [], fn step, line ->
+      next_y = if y - step < 0, do: 0, else: y - step
+
+      if x + step > width || y - step < 0 do
+        {:halt, line}
+      else
+        char = map |> Map.get(next_y, []) |> Enum.at(x + step)
+        {:cont, [char | line]}
+      end
+    end)
+    |> Enum.reverse()
   end
 end
--- test/Day_4/ceres_search_test.exs
+++ test/Day_4/ceres_search_test.exs
@@ -21,7 +21,7 @@ defmodule Day4.CeresSearchTest do
       count =
         Day4.CeresSearch.count_xmas(%{
           0 => ["X", "M", "A", "S"],
-          1 => ["X", "M", "A", "-"],
+          1 => ["X", "-", "-", "S"],
           2 => ["X", "M", "A", "-"],
           3 => ["X", "M", "A", "S"]
         })
@@ -29,32 +29,136 @@ defmodule Day4.CeresSearchTest do
       assert count == 2
     end
 
-    test "diagonal bottom right, 1" do
-      count = Day4.CeresSearch.count_xmas(%{
-        0 => ["X", "-", "-", "-"],
-        1 => ["-", "M", "-", "-"],
-        2 => ["-", "-", "A", "-"],
-        3 => ["-", "-", "-", "S"],
-      })
-      assert count == 1
+    test "diagonal bottom right, many" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["X", "-", "-", "-"],
+          1 => ["X", "M", "-", "-"],
+          2 => ["-", "M", "A", "-"],
+          3 => ["-", "-", "A", "S"],
+          4 => ["-", "-", "-", "S"]
+        })
+
+      assert count == 2
     end
 
     test "top to bottom" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["X", "X", "-"],
+          1 => ["-", "M", "X"],
+          2 => ["-", "A", "M"],
+          3 => ["-", "S", "A"],
+          4 => ["-", "-", "S"]
+        })
+
+      assert count == 2
     end
 
     test "diagonal bottom left" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["X", "-", "-", "X"],
+          1 => ["X", "-", "M", "X"],
+          2 => ["-", "A", "M", "-"],
+          3 => ["S", "A", "A", "S"],
+          4 => ["S", "-", "-", "S"]
+        })
+
+      assert count == 2
     end
 
-    test "left to right" do
+    test "right to left" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["X", "-", "-", "-"],
+          1 => ["S", "A", "M", "X"],
+          2 => ["-", "A", "M", "-"],
+          3 => ["S", "A", "M", "X"],
+          4 => ["-", "X", "-", "S"]
+        })
+
+      assert count == 2
     end
 
     test "diagonal top left" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["S", "-", "U", "X"],
+          1 => ["S", "A", "M", "A"],
+          2 => ["-", "A", "M", "-"],
+          3 => ["M", "A", "M", "X"],
+          4 => ["S", "-", "-", "X"]
+        })
+
+      assert count == 2
     end
 
     test "bottom to top" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["X", "S", "-"],
+          1 => ["-", "A", "S"],
+          2 => ["-", "M", "A"],
+          3 => ["-", "X", "M"],
+          4 => ["-", "X", "X"]
+        })
+
+      assert count == 2
     end
 
     test "diagonal top right" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["X", "S", "-", "S"],
+          1 => ["-", "A", "A", "S"],
+          2 => ["-", "M", "A", "A"],
+          3 => ["X", "M", "M", "M"],
+          4 => ["X", "-", "X", "-"]
+        })
+
+      assert count == 2
+    end
+
+    test "example 1" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => [".", ".", ".", ".", "X", "X", "M", "A", "S", "."],
+          1 => [".", "S", "A", "M", "X", "M", "S", ".", ".", "."],
+          2 => [".", ".", ".", "S", ".", ".", "A", ".", ".", "."],
+          3 => [".", ".", "A", ".", "A", ".", "M", "S", ".", "X"],
+          4 => ["X", "M", "A", "S", "A", "M", "X", ".", "M", "M"],
+          5 => ["X", ".", ".", ".", ".", ".", "X", "A", ".", "A"],
+          6 => ["S", ".", "S", ".", "S", ".", "S", ".", "S", "S"],
+          7 => [".", "A", ".", "A", ".", "A", ".", "A", ".", "A"],
+          8 => [".", ".", "M", ".", "M", ".", "M", ".", "M", "M"],
+          9 => [".", "X", ".", "X", ".", "X", "M", "A", "S", "X"]
+        })
+
+      assert count == 18
+    end
+
+    test "example 2" do
+      count =
+        Day4.CeresSearch.count_xmas(%{
+          0 => ["M", "M", "M", "S", "X", "X", "M", "A", "S", "M"],
+          1 => ["M", "S", "A", "M", "X", "M", "S", "M", "S", "A"],
+          2 => ["A", "M", "X", "S", "X", "M", "A", "A", "M", "M"],
+          3 => ["M", "S", "A", "M", "A", "S", "M", "S", "M", "X"],
+          4 => ["X", "M", "A", "S", "A", "M", "X", "A", "M", "M"],
+          5 => ["X", "X", "A", "M", "M", "X", "X", "A", "M", "A"],
+          6 => ["S", "M", "S", "M", "S", "A", "S", "X", "S", "S"],
+          7 => ["S", "A", "X", "A", "M", "A", "S", "A", "A", "A"],
+          8 => ["M", "A", "M", "M", "M", "X", "M", "M", "M", "M"],
+          9 => ["M", "X", "M", "X", "A", "X", "M", "A", "S", "X"]
+        })
+
+      assert count == 18
+    end
+
+    test "with input" do
+      count = Day4.CeresSearch.count_xmas_from_file("test/Day_4/input.txt")
+      assert count == 2536
     end
   end
 end
